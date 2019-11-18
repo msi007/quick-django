f1c9e5d15eabd92518987edfc94990738ef05e41
Mohidul Islam <mohidul.nu@gmail.com>
2019-11-18 19:22:42 +0600

    Add custom management command to rename project

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,17 +1,12 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 
-
-@login_required
-def home(request):
-    return render(request, 'home.html', )
+from core.views import home
 
 
 urlpatterns = [
-    path('home', home, name='home'),
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include("accounts.urls"))
 ]
